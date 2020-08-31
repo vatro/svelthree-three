314a56154b0a1c04e5aa09323b3d84fbdaaef998
Vatroslav Vrbanic <vatro@users.noreply.github.com>
2020-08-31 18:51:58 +0200

    Update XRHandModelFactory.d.ts

--- src/webxr/XRHandModelFactory.d.ts
+++ src/webxr/XRHandModelFactory.d.ts
@@ -1,34 +1,30 @@
-import { Object3D } from '../core/Object3D.js';
-import { Texture } from '../textures/Texture.js';
-import { Mesh } from '../objects/Mesh.js';
+import { Object3D } from "../core/Object3D.js";
+import { Texture } from "../textures/Texture.js";
+import { Mesh } from "../objects/Mesh.js";
 
-import {
-	XRHandPrimitiveModel
-} from './XRHandPrimitiveModel.js';
+import { XRHandPrimitiveModel } from "./XRHandPrimitiveModel.js";
 
-import {
-	XRHandOculusMeshModel
-} from './XRHandOculusMeshModel.js';
-
+import { XRHandOculusMeshModel } from "./XRHandOculusMeshModel.js";
 
 export class XRHandModel extends Object3D {
+  constructor(controller: Group);
 
-	constructor(controller:Group);
+  controller: Group;
+  motionController: XRHandPrimitiveModel | XRHandOculusMeshModel;
+  envMap: Texture;
+  mesh: Mesh;
+  xrInputSource: any;
 
-    controller: Group;
-    motionController: XRHandPrimitiveModel | XRHandOculusMeshModel;
-    envMap:Texture
-    mesh:Mesh
-    xrInputSource:any
-
-	updateMatrixWorld( force: boolean ): void;
+  updateMatrixWorld(force: boolean): void;
 }
 
 export class XRHandModelFactory {
+  constructor();
+  path: string;
 
-	constructor( );
-	path: string;
-
-	createControllerModel( controller: Group ): XRHandModel;
-
+  createHandModel(
+    controller: Group,
+    profile: string,
+    options: { [key: string]: any }
+  ): XRHandModel;
 }
