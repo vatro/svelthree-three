e566f6746c0a5f69c631457845b142040eb9a002
Vatroslav Vrbanic <vatro@users.noreply.github.com>
2020-08-31 18:32:33 +0200

    Create XRHandModelFactory.d.ts

---
+++ src/webxr/XRHandModelFactory.d.ts
@@ -0,0 +1,34 @@
+import { Object3D } from '../core/Object3D.js';
+import { Texture } from '../textures/Texture.js';
+import { Mesh } from '../objects/Mesh.js';
+
+import {
+	XRHandPrimitiveModel
+} from './XRHandPrimitiveModel.js';
+
+import {
+	XRHandOculusMeshModel
+} from './XRHandOculusMeshModel.js';
+
+
+export class XRHandModel extends Object3D {
+
+	constructor(controller:Group);
+
+    controller: Group;
+    motionController: XRHandPrimitiveModel | XRHandOculusMeshModel;
+    envMap:Texture
+    mesh:Mesh
+    xrInputSource:any
+
+	updateMatrixWorld( force: boolean ): void;
+}
+
+export class XRHandModelFactory {
+
+	constructor( );
+	path: string;
+
+	createControllerModel( controller: Group ): XRHandModel;
+
+}
